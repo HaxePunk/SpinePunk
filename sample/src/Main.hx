4bbef3f3bf58aa339673e30fff4e20fb92e76b7c
Ben Morris <ben@bendmorris.com>
2018-02-08 08:43:39 -0800

    Update sample

--- sample/src/Main.hx
+++ sample/src/Main.hx
@@ -1,15 +1,10 @@
 import haxepunk.Engine;
 import haxepunk.HXP;
-import haxepunk.RenderMode;
 import flash.events.Event;
 import flash.display.BitmapData;
 import flash.geom.Point;
 
 class Main extends Engine {
-    public function new(width : Int=0, height : Int=0, frameRate : Float=60, fixed : Bool=false, ?renderMode : RenderMode) {
-        super(width, height, frameRate, fixed, renderMode);
-    }
-
     override public function init()
     {
 #if debug
--- sample/src/MainScene.hx
+++ sample/src/MainScene.hx
@@ -1,7 +1,8 @@
 import haxepunk.HXP;
 import haxepunk.Entity;
 import haxepunk.Scene;
-import haxepunk.utils.Input;
+import haxepunk.assets.AssetCache;
+import haxepunk.input.Mouse;
 import spinehaxe.SkeletonData;
 import spinehaxe.Bone;
 import spinehaxe.animation.AnimationState;
@@ -17,17 +18,9 @@ import flash.display.Sprite;
 import flash.events.Event;
 import flash.events.MouseEvent;
 
-#if openfl
-import openfl.Assets;
-import openfl.display.FPS;
-#else
-import nme.Assets;
-import nme.display.FPS;
-#end
-
 class MainScene extends Scene {
-	static var animations = ["stand", "walk", "run", "attack unarmed 1", "attack unarmed 2", "attack unarmed 1", "attack unarmed 3", "death", "revive"];
-	static var loop = [true, true, true, false, false, false, false, false, false];
+	static var animations = ["walk"];
+	static var loop = [true];
 	var on:Int = 0;
 
 	var atlas:Atlas;
@@ -38,15 +31,15 @@ class MainScene extends Scene {
 	var mode:Int = 0;
 
 	public override function begin() {
-		atlas = new Atlas(Assets.getText("assets/humanoid.atlas"), new BitmapDataTextureLoader("assets/"));
+		atlas = new Atlas(assetCache.getText("assets/goblins.atlas"), new BitmapDataTextureLoader("assets/"));
 		var json = new SkeletonJson(new AtlasAttachmentLoader(atlas));
-		var skeletonData:SkeletonData = json.readSkeletonData(Assets.getText("assets/humanoid.json"), "humanoid");
+		var skeletonData:SkeletonData = json.readSkeletonData(assetCache.getText("assets/goblins.json"), "goblins");
 
 		var stateData = new AnimationStateData(skeletonData);
 		stateData.defaultMix = 0.2;
 
 		skeleton = new SpinePunk(skeletonData, stateData);
-		skeleton.skin = 'elf';
+		skeleton.skin = 'goblin';
 
 		state = new AnimationState(stateData);
 		state.setAnimationByName(0, animations[0], true);
@@ -66,7 +59,7 @@ class MainScene extends Scene {
 	}
 
 	public override function update() {
-		if (Input.mousePressed) onClick();
+		if (Mouse.mousePressed) onClick();
 		super.update();
 	}
 }
